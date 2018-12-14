36f3bfefbfad8985158e1f7966fb09ab3d8c1ed6
Dima <ddd89851151809@gmail.com>
2018-12-14 11:40:05 +0300

    gg

--- Спец Задания/Spiral walk.py
+++ Спец Задания/Spiral walk.py
@@ -1,15 +1,24 @@
-from random import random
-N = 5
-M = 6
-a = []
-n = 0
-for i in range(N):
-    z = []
-    for j in range(M):
-        n += 1
-        z.append(n)
-        print("%4d" % n, end='')
-    print()
-    a.append(z)
-print()
- 
+n = int(input())
+mat = [[0]*n for i in range(n)]
+st, m = 1, 0
+
+mat[n//2][n//2]=n*n
+
+for v in range(n//2):
+    for i in range(n-m):
+        mat[v][i+v] = st
+        st+=1 
+    for i in range(v+1, n-v):
+        mat[i][-v-1] = st
+        st+=1
+    for i in range(v+1, n-v):
+        mat[-v-1][-i-1] =st
+        st+=1
+    for i in range(v+1, n-(v+1)):
+        mat[-i-1][v]=st
+        st+=1
+    m+=2
+
+#Вывод результата на экран
+for i in mat:
+    print(*i)
